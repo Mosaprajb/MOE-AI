5a0043abf66957e71c0a612fb14de5d5cf93dba3
Mosaprajb <99310662+Mosaprajb@users.noreply.github.com>
2026-07-25 01:08:47 -0500

    Export opportunity execution service

--- worker/src/core/index.js
+++ worker/src/core/index.js
@@ -4,3 +4,4 @@ export * from './context-engine.js';
 export * from './decision-pipeline.js';
 export * from './moerand-runtime.js';
 export * from './opportunity-alert-service.js';
+export * from './execution-service.js';
